e7c509abcddb5df3ae21e9e1b4ef3f3c77bc4cec
root <root@ccook1.mylabserver.com>
2015-08-05 16:07:25 +0000

    Commit chef-repo for the first time

---
+++ cookbooks/apache/attributes/default.rb
@@ -0,0 +1,10 @@
+default["apache"]["sites"]["ccook2"] = { "site_title" => "Cook2 websites coming soon", "port" => 80, "domain" => "ccook2.mylabserver.com" }
+default["apache"]["sites"]["ccook2b"] = { "site_title" => "Cook2b is coming soon!!","port" => 80, "domain" => "ccook2b.mylabserver.com" }
+default["apache"]["sites"]["ccook3"] = { "site_title" => "Cook3 website","port" => 80, "domain" => "ccook3.mylabserver.com" }
+
+case node["platform"]
+when "centos"
+	default["apache"]["package"] = "httpd"
+when "ubuntu"
+	default["apache"]["package"] = "apache2"
+end
