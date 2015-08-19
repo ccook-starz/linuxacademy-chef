00a9dc37982100ceff1d6e442ece0cb393782fa6
root <root@ccook1.mylabserver.com>
2015-08-19 18:34:40 +0000

    Updating so I can install a site cookbook

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -2,6 +2,8 @@ default["apache"]["sites"]["ccook2"] = { "site_title" => "Cook2 websites coming 
 default["apache"]["sites"]["ccook2b"] = { "site_title" => "Cook2b is coming soon!!","port" => 80, "domain" => "ccook2b.mylabserver.com" }
 default["apache"]["sites"]["ccook3"] = { "site_title" => "Cook3 website","port" => 80, "domain" => "ccook3.mylabserver.com" }
 
+default["author"]["name"] = "Chad"
+
 case node["platform"]
 when "centos"
 	default["apache"]["package"] = "httpd"
